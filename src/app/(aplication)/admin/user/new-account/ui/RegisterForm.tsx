679e6895d1c9c9f64a950bda04135cb443197f89
Danyael Vásquez Ojeda <d.vsquezojeda@uandresbello.edu>
2025-11-07 19:33:28 -0300

    FIX changes before deploy 1

--- src/app/(aplication)/admin/user/new-account/ui/RegisterForm.tsx
+++ src/app/(aplication)/admin/user/new-account/ui/RegisterForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-//  import { login } from "@/actions"; //registerUser
+//    import { login } from "@/actions"; //registerUser
 import clsx from "clsx";
 import Link from "next/link";
 import { useState } from "react";
